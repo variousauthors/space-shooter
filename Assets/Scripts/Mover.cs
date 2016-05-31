e15dda7d1ed761b8f218cb38889b881cb468f900
various.authors <various.authors@gmail.com>
2016-05-30 17:21:08 -0700

    lesson 6: creating shots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	private Rigidbody rb;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+	
+		rb.velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
